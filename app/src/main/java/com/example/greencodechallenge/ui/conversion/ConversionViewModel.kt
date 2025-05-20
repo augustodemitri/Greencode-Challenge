311bb4ea712d6bd700b8b8ab7307d950c8bc47b2
guillermo <augustodemitri52@gmail.com>
2025-05-20 15:08:51 -0300

    feature: implement currency conversion UI with i18n support

---
+++ app/src/main/java/com/example/greencodechallenge/ui/conversion/ConversionViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.greencodechallenge.ui.conversion
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ConversionViewModel @Inject constructor() : ViewModel() {
+    // TODO: Implementar logica
+} 
